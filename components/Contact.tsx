f62dc730786433a539d3b25e491e5cc35808af1d
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-12-27 23:20:34 +0600

    eslintrcjson

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
     <section id="contact"
     className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
-      <p className="text-textGreen text-lg items-center font-titleFont font-semibold flex tracking-wide">What's Next</p>
+      <p className="text-textGreen text-lg items-center font-titleFont font-semibold flex tracking-wide">Whats Next</p>
       <h2 className="font-tileFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[556px] text-center text-textDark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatem reprehenderit architecto dolorem error impedit commodi hic fugit, eligendi, eos nihil. Saepe optio alias dolorem quis illo fugiat unde commodi!</p>
       <a href="mailto:er.ibrahimsarder@gmail.com" target="-blank"><button
